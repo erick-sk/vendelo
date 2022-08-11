3f1151ff9c5afa256b89f26d1618745e33d30fec
Erick Saavedra <esvdrah@gmail.com>
2022-08-11 12:38:16 -0500

    Fix validation categories table, and add translates

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   def index
     @categories = Category.order(name: :asc).load_async
 
-    @pagy, @products = pagy_countless(FindProducts.new.call(params).load_async, items: 12)
+    @pagy, @products = pagy_countless(FindProducts.new.call(product_params_index).load_async, items: 12)
   end
 
   def show
@@ -50,4 +50,7 @@ class ProductsController < ApplicationController
       params.require(:product).permit(:title, :category_id, :description, :price, :photo)
     end
 
+    def product_params_index
+      params.permit(:category_id, :min_price, :max_price, :query_text, :order_by, :page)
+    end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
+  # Validates
+  validates :name, presence: true
+
   # Associations
   has_many :products, dependent: :restrict_with_exception
 end

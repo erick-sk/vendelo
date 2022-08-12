43da7da68a99bf4ece01b72d030c5d3d6281e651
Erick Saavedra <esvdrah@gmail.com>
2022-08-11 20:33:25 -0500

    Add model User and create new users

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ApplicationRecord
+  # SecurePassword
+  has_secure_password
+
+  # Validates
+  validates :email, presence: true, uniqueness: true,
+            format: {
+              with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+              message: :invalid
+            }
+  validates :username, presence: true, uniqueness: true,
+            length:  { in: 3..15 },
+            format: {
+              with: /\A[a-z0-9A-Z]+\z/,
+              message: :invalid
+            }
+  validates :password, length: { minimum: 6 }
+
+  # Callbacks
+  before_save :downcase_attributes
+
+  private
+
+    def downcase_attributes
+      self.email = email.downcase
+      self.username = username.downcase
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,8 @@ Rails.application.routes.draw do
 
   resources :categories, except: :show
   resources :products, path: '/'
+
+  namespace :authentication, path: '', as: '' do
+    resources :users, only: [:new, :create]
+  end
 end

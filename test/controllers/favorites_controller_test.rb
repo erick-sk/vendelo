e3b7d54598648a16030dae8fbab4ea9691f8f218
Erick Saavedra <esvdrah@gmail.com>
2022-08-18 15:37:13 -0500

    Fix test favorites_controller_test

--- test/controllers/favorites_controller_test.rb
+++ test/controllers/favorites_controller_test.rb
@@ -3,14 +3,23 @@ require 'test_helper'
 class FavoritesControllerTest < ActionDispatch::IntegrationTest
   setup do
     login
-    @product = products(:megadrive)
+    @megadrive = products(:megadrive)
+    @ps5 = products(:ps5)
   end
 
   test 'should create favorite' do
     assert_difference('Favorite.count', 1) do
-      post favorites_url(product_id: @product.id)
+      post favorites_url(product_id: @megadrive.id)
     end
 
-    assert_redirected_to product_path(@product)
+    assert_redirected_to product_path(@megadrive)
+  end
+
+  test 'should remove favorite' do
+    assert_difference('Favorite.count', -1) do
+      delete favorite_url(@ps5.id)
+    end
+
+    assert_redirected_to product_path(@ps5)
   end
 end
